dc1c98a0f4bc940dc67a6d44e953895d73cf0ed8
maheshwar4444 <103100700+maheshwar4444@users.noreply.github.com>
2022-10-19 11:48:28 +0530

    Added adddelete

---
+++ AddDelete/src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+
+	let peopleTask=[];
+	let personTask;		
+	let yes = false;
+
+
+	const handleInput = ()=> {
+		peopleTask=peopleTask.concat(personTask)
+		personTask='';
+		
+		
+	}
+
+	const handleClick= (task) => {
+		peopleTask=peopleTask.filter((ele)=> ele!=task)
+		personTask='';
+		
+	
+		
+	}
+
+</script>
+
+	<div>
+		<label for="text">Add New Task</label>
+		<input type="text" bind:value={personTask}>
+		<button on:click={handleInput}>Add</button>
+		
+
+	</div>
+	
+	{#each peopleTask as task}
+	
+	  {task} <button on:click={ ()=> handleClick (task)} > Delete </button>
+	  
+	  <br>
+	  <label for="yes">{task? "Completed" : "Not Completed"}
+		<input type=checkbox bind:checked={yes}>
+		{yes}
+		
+	</label>
+	
+{/each}
+
+
+
+
+
+
+
